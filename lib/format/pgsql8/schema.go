a9fceea67260d782ab7df7aa189f3b4bf78e7662
Austin Hyde <austin109@gmail.com>
2021-01-05 16:50:11 -0500

    pgsql8 Schema.GetCreationSql

--- lib/format/pgsql8/schema.go
+++ lib/format/pgsql8/schema.go
@@ -1,6 +1,10 @@
 package pgsql8
 
 import (
+	"strings"
+
+	"github.com/dbsteward/dbsteward/lib"
+	"github.com/dbsteward/dbsteward/lib/format/pgsql8/sql"
 	"github.com/dbsteward/dbsteward/lib/model"
 	"github.com/dbsteward/dbsteward/lib/output"
 )
@@ -15,6 +19,23 @@ func NewSchema() *Schema {
 }
 
 func (self *Schema) GetCreationSql(schema *model.Schema) []output.ToSql {
-	// TODO(go,pgsql)
-	return nil
+	// don't create the public schema
+	if strings.EqualFold(schema.Name, "public") {
+		return nil
+	}
+
+	ddl := []output.ToSql{
+		&sql.SchemaCreate{schema.Name},
+	}
+
+	if schema.Owner != "" {
+		owner := lib.GlobalXmlParser.RoleEnum(lib.GlobalDBSteward.NewDatabase, schema.Owner)
+		ddl = append(ddl, &sql.SchemaAlterOwner{schema.Name, owner})
+	}
+
+	if schema.Description != "" {
+		ddl = append(ddl, &sql.SchemaSetComment{schema.Name, schema.Description})
+	}
+
+	return ddl
 }
---
+++ lib/format/pgsql8/sql/schema.go
@@ -0,0 +1,33 @@
+package sql
+
+import (
+	"fmt"
+
+	"github.com/dbsteward/dbsteward/lib/output"
+)
+
+type SchemaCreate struct {
+	Schema string
+}
+
+func (self *SchemaCreate) ToSql(q output.Quoter) string {
+	return fmt.Sprintf("CREATE SCHEMA %s;", q.QuoteSchema(self.Schema))
+}
+
+type SchemaAlterOwner struct {
+	Schema string
+	Owner  string
+}
+
+func (self *SchemaAlterOwner) ToSql(q output.Quoter) string {
+	return fmt.Sprintf("ALTER SCHEMA %s OWNER TO %s;", q.QuoteSchema(self.Schema), q.QuoteRole(self.Owner))
+}
+
+type SchemaSetComment struct {
+	Schema  string
+	Comment string
+}
+
+func (self *SchemaSetComment) ToSql(q output.Quoter) string {
+	return fmt.Sprintf("COMMENT ON SCHEMA %s IS %s;", q.QuoteSchema(self.Schema), q.LiteralStringEscaped(self.Comment))
+}
