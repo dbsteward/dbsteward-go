07a15f317c6bca8c58da5d38f2174f3eebabfbcb
Austin Hyde <austin109@gmail.com>
2021-01-25 19:52:11 -0500

    pgsql CREATE TRIGGER does not support qualified trigger names

--- lib/format/pgsql8/sql/trigger.go
+++ lib/format/pgsql8/sql/trigger.go
@@ -21,7 +21,7 @@ type TriggerCreate struct {
 func (self *TriggerCreate) ToSql(q output.Quoter) string {
 	return fmt.Sprintf(
 		"CREATE TRIGGER %s\n  %s %s\n  ON %s\n  FOR EACH %s\n  EXECUTE PROCEDURE %s;",
-		self.Trigger.Qualified(q),
+		self.Trigger.Quoted(q),
 		self.Timing,
 		strings.Join(self.Events, " OR "),
 		self.Table.Qualified(q),
