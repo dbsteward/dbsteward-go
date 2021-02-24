416a12f9935ed90f00f5061330e6fdef19b1b1d5
Austin Hyde <austin109@gmail.com>
2021-02-24 11:11:16 -0500

    pgsql8 add table_test and table options tests

---
+++ lib/format/pgsql8/table_test.go
@@ -0,0 +1,64 @@
+package pgsql8_test
+
+import (
+	"testing"
+
+	"github.com/dbsteward/dbsteward/lib/format/pgsql8"
+	"github.com/dbsteward/dbsteward/lib/format/pgsql8/sql"
+	"github.com/dbsteward/dbsteward/lib/model"
+	"github.com/dbsteward/dbsteward/lib/output"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTable_GetCreationSql_TableOptions(t *testing.T) {
+	schema := &model.Schema{
+		Name: "public",
+		Tables: []*model.Table{
+			&model.Table{
+				Name:        "test",
+				PrimaryKey:  model.DelimitedList{"id"},
+				Description: "test description",
+				Columns: []*model.Column{
+					{Name: "id", Type: "int"},
+					{Name: "foo", Type: "int"},
+				},
+				TableOptions: []*model.TableOption{
+					&model.TableOption{
+						SqlFormat: model.SqlFormatPgsql8,
+						Name:      "tablespace",
+						Value:     "schmableschpace",
+					},
+					&model.TableOption{
+						SqlFormat: model.SqlFormatPgsql8,
+						Name:      "with",
+						Value:     "(oids=true,fillfactor=70)",
+					},
+					&model.TableOption{
+						SqlFormat: model.SqlFormatMysql5,
+						Name:      "auto_increment",
+						Value:     "5",
+					},
+				},
+			},
+		},
+	}
+
+	ddl := pgsql8.NewTable().GetCreationSql(schema, schema.Tables[0])
+	assert.Equal(t, []output.ToSql{
+		&sql.TableCreate{
+			Table: sql.TableRef{"public", "test"},
+			Columns: []sql.ColumnDefinition{
+				{Name: "id", Type: sql.TypeRef{"", "int"}},
+				{Name: "foo", Type: sql.TypeRef{"", "int"}},
+			},
+			OtherOptions: []sql.TableCreateOption{
+				{Option: "tablespace", Value: "schmableschpace"},
+				{Option: "with", Value: "(oids=true,fillfactor=70)"},
+			},
+		},
+		&sql.TableSetComment{
+			Table:   sql.TableRef{"public", "test"},
+			Comment: "test description",
+		},
+	}, ddl)
+}
