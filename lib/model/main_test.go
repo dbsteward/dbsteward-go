3508162aa0c194c1702e9119dab700f30157a120
Austin Hyde <austin109@gmail.com>
2021-03-08 15:16:43 -0500

    force code coverage results for model directory by adding no-op TestMain

---
+++ lib/model/main_test.go
@@ -0,0 +1,12 @@
+package model_test
+
+import (
+	"os"
+	"testing"
+)
+
+// HACK: this exists solely to force code coverage metrics in the model package
+
+func TestMain(m *testing.M) {
+	os.Exit(m.Run())
+}
