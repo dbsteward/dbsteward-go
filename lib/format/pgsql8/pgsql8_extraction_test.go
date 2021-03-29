afc13cdd791ed0c7ab5a78f3eb929a6ef90d48fa
Austin Hyde <austin109@gmail.com>
2021-03-29 13:29:12 -0400

    pgsql8 extraction test complicated indexes

--- lib/format/pgsql8/pgsql8_extraction_test.go
+++ lib/format/pgsql8/pgsql8_extraction_test.go
@@ -12,6 +12,8 @@ import (
 	"github.com/dbsteward/dbsteward/lib/model"
 )
 
+// TODO(go,3) is there a way to make this set of tests a whole lot less annoying?
+
 func TestOperations_ExtractSchema_FunctionalIndex(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	introspector := live.NewMockIntrospector(ctrl)
@@ -35,22 +37,53 @@ func TestOperations_ExtractSchema_FunctionalIndex(t *testing.T) {
 	}, nil)
 	introspector.EXPECT().GetColumns("public", "test").Return([]live.ColumnEntry{
 		live.ColumnEntry{
-			Name:     "name",
+			Name:     "col1",
 			AttrType: "text",
 			Position: 1,
+		},
+		live.ColumnEntry{
+			Name:     "col2",
+			AttrType: "text",
+			Position: 2,
+		},
+		live.ColumnEntry{
+			Name:     "col3",
+			AttrType: "text",
+			Position: 3,
 		},
 	}, nil)
 	introspector.EXPECT().GetIndexes("public", "test").Return([]live.IndexEntry{
 		live.IndexEntry{
-			Name:       "lower_idx",
-			Dimensions: []string{"lower(name)"},
+			Name: "testidx",
+			Dimensions: []string{
+				"lower(col1)",
+				"col2",
+				"(col1 || ';;'::text)",
+				"col3",
+				"\"overlay\"(btrim(col2), 'x'::text, 2)",
+			},
 		},
 	}, nil)
 
 	schema := commonExtract(introspector)
-	assert.Equal(t, "lower_idx_1", schema.Tables[0].Indexes[0].Dimensions[0].Name)
-	assert.Equal(t, "lower(name)", schema.Tables[0].Indexes[0].Dimensions[0].Value)
+
+	// TODO(feat) test changing Using
+	// TODO(feat) test conditional index
+	// TODO(feat) test unique index
 	// TODO(feat) assert that .Sql = true
+	expected := &model.Index{
+		Name:  "testidx",
+		Using: "btree",
+		Dimensions: []*model.IndexDim{
+			{Name: "testidx_1", Value: "lower(col1)"},
+			{Name: "testidx_2", Value: "col2"},
+			{Name: "testidx_3", Value: "(col1 || ';;'::text)"},
+			{Name: "testidx_4", Value: "col3"},
+			{Name: "testidx_5", Value: "\"overlay\"(btrim(col2), 'x'::text, 2)"},
+		},
+	}
+	// test the full slice of indexes to ensure we don't do something weird like duplicate/split the index
+	assert.Equal(t, []*model.Index{expected}, schema.Tables[0].Indexes)
 }
 
 func commonExtract(introspector *live.MockIntrospector) *model.Schema {
