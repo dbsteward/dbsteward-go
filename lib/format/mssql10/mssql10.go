27366328daa22f6b52733070e9005dd12d8e3a3b
Bill Moran <william.p.moran.jr@gmail.com>
2024-04-23 06:35:15 -0400

    Remove mssql support until the core code is stabalized

--- lib/format/mssql10/mssql10.go
+++
@@ -1,8 +0,0 @@
-package mssql10
-
-import "github.com/dbsteward/dbsteward/lib/format"
-
-var GlobalOperations = NewOperations()
-var GlobalLookup = &format.Lookup{
-	Operations: GlobalOperations,
-}
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/dbsteward/dbsteward/lib"
 	"github.com/dbsteward/dbsteward/lib/format"
-	"github.com/dbsteward/dbsteward/lib/format/mssql10"
 	"github.com/dbsteward/dbsteward/lib/format/mysql5"
 	"github.com/dbsteward/dbsteward/lib/format/pgsql8"
 	"github.com/dbsteward/dbsteward/lib/ir"
@@ -12,9 +11,8 @@ import (
 func main() {
 	// correlates to bin/dbsteward
 	lib.GlobalDBSteward = lib.NewDBSteward(format.LookupMap{
-		ir.SqlFormatPgsql8:  pgsql8.GlobalLookup,
-		ir.SqlFormatMysql5:  mysql5.GlobalLookup,
-		ir.SqlFormatMssql10: mssql10.GlobalLookup,
+		ir.SqlFormatPgsql8: pgsql8.GlobalLookup,
+		ir.SqlFormatMysql5: mysql5.GlobalLookup,
 	})
 	lib.GlobalDBSteward.ArgParse()
 	lib.GlobalDBSteward.Notice("Done")
